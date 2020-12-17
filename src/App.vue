6b29e70f291cc4b0bf8f53248e99c87225c28e5e
Felipe90 <lordganondorf@gmail.com>
2020-12-17 14:01:05 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img v-bind:alt="name" v-bind:src="imgPath" />
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "Bitcoin",
+      imgPath: "../assest/bitcoin-btc-logo.png",
+      changePercent: 10,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
